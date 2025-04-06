d7e401047cd5102c6d10c12fc524eb89ff4b4daa
discotechnology <drosenfeld@csumb.edu>
2025-04-06 16:17:54 -0700

    Set up view binding

--- app/src/main/java/com/example/cst338hw4_gymlog/MainActivity.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cst338hw4_gymlog;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.cst338hw4_gymlog.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
