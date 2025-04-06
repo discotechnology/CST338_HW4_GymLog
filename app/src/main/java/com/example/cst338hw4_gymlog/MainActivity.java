2da6a76e11fe3540e4e8df5e54924eb3d5f59b40
discotechnology <drosenfeld@csumb.edu>
2025-04-06 16:27:51 -0700

    Added method to get info from display

--- app/src/main/java/com/example/cst338hw4_gymlog/MainActivity.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cst338hw4_gymlog;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,10 @@ import com.example.cst338hw4_gymlog.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    String exercise;
+    double weight;
+    int reps;
+
     ActivityMainBinding binding;
 
     @Override
@@ -22,6 +27,28 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();
+            }
+        });
+    }
 
+    private void getInformationFromDisplay() {
+
+        exercise = binding.exerciseInputEditText.getText().toString();
+
+        try {
+            weight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d("DAC_GYMLOG", "Error reading value from Weight");
+        }
+
+        try {
+            weight = Integer.parseInt(binding.repsInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d("DAC_GYMLOG", "Error reading value from Weight");
+        }
     }
 }
