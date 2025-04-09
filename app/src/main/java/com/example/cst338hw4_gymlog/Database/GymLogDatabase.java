26fe03718d5107c135799a5cbacb77246f1454a8
discotechnology <drosenfeld@csumb.edu>
2025-04-09 13:03:46 -0700

    Added code to manage threads in GymLogDatabase

--- app/src/main/java/com/example/cst338hw4_gymlog/Database/GymLogDatabase.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/Database/GymLogDatabase.java
@@ -5,7 +5,16 @@ import androidx.room.RoomDatabase;
 
 import com.example.cst338hw4_gymlog.Database.Entities.GymLog;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {GymLog.class}, version = 1, exportSchema = false)
 public abstract class GymLogDatabase extends RoomDatabase {
+
     public static final String gymLogTable = "gymLogTable";
+
+    private static volatile GymLogDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 }
