d49ece00902673aabd6b0411dfe3bafa987ce242
discotechnology <drosenfeld@csumb.edu>
2025-04-09 13:37:19 -0700

    Updated getAllLogs() to work with future

--- app/src/main/java/com/example/cst338hw4_gymlog/Database/GymLogRepository.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/Database/GymLogRepository.java
@@ -1,10 +1,17 @@
 package com.example.cst338hw4_gymlog.Database;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.example.cst338hw4_gymlog.Database.Entities.GymLog;
+import com.example.cst338hw4_gymlog.MainActivity;
 
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import kotlin.reflect.KCallable;
 
 public class GymLogRepository {
 
@@ -18,6 +25,18 @@ public class GymLogRepository {
     }
 
     public ArrayList<GymLog> getAllLogs() {
-        return allLogs;
+        Future<ArrayList<GymLog>> future = GymLogDatabase.databaseWriteExecutor.submit(new Callable<ArrayList<GymLog>>() {
+            @Override
+            public ArrayList<GymLog> call() throws Exception {
+                return gymLogDAO.getAllRecords();
+            }
+        });
+        try {
+            return future.get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            Log.i(MainActivity.TAG, "Problem retrieving GymLogs from repository.");
+        }
+        return null;
     }
 }
