f1d15cd5596662bc5376e0174b10d6ee63ecf320
discotechnology <drosenfeld@csumb.edu>
2025-04-09 15:51:57 -0700

    Wrote code to make repository singleton

--- app/src/main/java/com/example/cst338hw4_gymlog/database/GymLogRepository.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/database/GymLogRepository.java
@@ -16,13 +16,18 @@ public class GymLogRepository {
     private GymLogDAO gymLogDAO;
     private ArrayList<GymLog> allLogs;
 
-    public GymLogRepository(Application application) {
+    private static GymLogRepository repository;
+
+    private GymLogRepository(Application application) {
         GymLogDatabase db = GymLogDatabase.getGymLogDatabase(application);
         this.gymLogDAO = db.gymLogDAO();
         this.allLogs = (ArrayList<GymLog>) this.gymLogDAO.getAllRecords();
     }
 
     public static GymLogRepository getRepository(Application application) {
+        if (repository != null) {
+            return repository;
+        }
         Future<GymLogRepository> future = GymLogDatabase.databaseWriteExecutor.submit(new Callable<GymLogRepository>() {
             @Override
             public GymLogRepository call() throws Exception {
