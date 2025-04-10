4702338db75d8c26d74e98781ed8d367606d1ccc
discotechnology <drosenfeld@csumb.edu>
2025-04-10 14:23:22 -0700

    Added default users with write executor in RoomDatabase.Callback

--- app/src/main/java/com/example/cst338hw4_gymlog/database/GymLogDatabase.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/database/GymLogDatabase.java
@@ -52,7 +52,16 @@ public abstract class GymLogDatabase extends RoomDatabase {
         public void onCreate(@NonNull SupportSQLiteDatabase db) {
             super.onCreate(db);
             Log.i(MainActivity.TAG, "DATABASE CREATED!!!!!");
-            //TODO; add databaseWriteExector.execute(() -> {...})
+            databaseWriteExecutor.execute(() -> {
+                UserDAO dao = INSTANCE.userDAO();
+                dao.deleteAll();
+                User admin = new User("admin1", "admin1");
+                admin.setAdmin(true);
+                dao.insert(admin);
+
+                User testUser1 = new User("testuser1", "testuser1");
+                dao.insert(testUser1);
+            });
         }
     };
 
