288dd2056364fdd181cc91441f3e565e80322d3a
discotechnology <drosenfeld@csumb.edu>
2025-04-10 13:53:13 -0700

    Added getters/setters, equals/hashcode, constructor to User

--- app/src/main/java/com/example/cst338hw4_gymlog/database/entities/User.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/database/entities/User.java
@@ -5,6 +5,8 @@ import androidx.room.PrimaryKey;
 
 import com.example.cst338hw4_gymlog.database.GymLogDatabase;
 
+import java.util.Objects;
+
 @Entity(tableName = GymLogDatabase.USER_TABLE)
 public class User {
 
@@ -13,4 +15,55 @@ public class User {
     private String username;
     private String password;
     private boolean isAdmin;
+
+    public User(String password, String username) {
+        this.password = password;
+        this.username = username;
+        isAdmin = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && isAdmin == user.isAdmin && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, isAdmin);
+    }
 }
