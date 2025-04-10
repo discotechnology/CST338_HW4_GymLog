856fc99df70dcc4af9ef5f46e0cb3c55607562af
discotechnology <drosenfeld@csumb.edu>
2025-04-10 13:46:53 -0700

    Added members to User

--- app/src/main/java/com/example/cst338hw4_gymlog/database/entities/User.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/database/entities/User.java
@@ -1,4 +1,10 @@
 package com.example.cst338hw4_gymlog.database.entities;
 
+import androidx.room.Entity;
+
 public class User {
+    private int id;
+    private String username;
+    private String password;
+    private boolean isAdmin;
 }
