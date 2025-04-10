e0ec93432c0ebfb90c7c7980eab996b10a849bbf
discotechnology <drosenfeld@csumb.edu>
2025-04-10 15:49:12 -0700

    Set up binding for LoginActivity

--- app/src/main/java/com/example/cst338hw4_gymlog/LoginActivity.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/LoginActivity.java
@@ -2,23 +2,18 @@ package com.example.cst338hw4_gymlog;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.cst338hw4_gymlog.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
