9286641e3826cb4a1960c4734bda23698bd38b97
discotechnology <drosenfeld@csumb.edu>
2025-04-10 13:57:14 -0700

    Created UserDAO

---
+++ app/src/main/java/com/example/cst338hw4_gymlog/database/UserDAO.java
@@ -0,0 +1,25 @@
+package com.example.cst338hw4_gymlog.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.cst338hw4_gymlog.database.entities.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(User... user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("select * from " + GymLogDatabase.USER_TABLE + " order by username")
+    List<User> getAllUsers();
+
+}
