e72a501dbead4b7a3f57567a771a17cbde725662
discotechnology <drosenfeld@csumb.edu>
2025-04-12 10:24:07 -0700

    Added repository member to LoginActivity

--- app/src/main/java/com/example/cst338hw4_gymlog/LoginActivity.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/LoginActivity.java
@@ -7,17 +7,21 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cst338hw4_gymlog.database.GymLogRepository;
 import com.example.cst338hw4_gymlog.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private GymLogRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        repository = GymLogRepository.getRepository(getApplication());
 
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
