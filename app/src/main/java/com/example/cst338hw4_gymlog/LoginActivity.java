fe89a2f379294b5340bde8897c473bda16192443
discotechnology <drosenfeld@csumb.edu>
2025-04-12 10:31:29 -0700

    Added method to verify user in LoginActivity

--- app/src/main/java/com/example/cst338hw4_gymlog/LoginActivity.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/LoginActivity.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cst338hw4_gymlog.database.GymLogRepository;
+import com.example.cst338hw4_gymlog.database.entities.User;
 import com.example.cst338hw4_gymlog.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -26,10 +28,26 @@ public class LoginActivity extends AppCompatActivity {
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
-                startActivity(intent);
+                if(!verifyUser()){
+                    Toast.makeText(LoginActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    private boolean verifyUser() {
+        String username = binding.userNameLoginEditText.getText().toString();
+        User user = repository.getUserByUsername(username);
+
+        String password = binding.passwordLoginEditText.getText().toString();
+        if(user.getPassword().equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     static Intent loginIntentFactory(Context context) {
