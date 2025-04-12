3bb3710276ea9407fa75d5b9c62d659db81cf2e9
discotechnology <drosenfeld@csumb.edu>
2025-04-12 16:55:32 -0700

    Added onSaveInstanceState()

--- app/src/main/java/com/example/cst338hw4_gymlog/MainActivity.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "DAC_GYMLOG";
     public static final String MAIN_ACTIVITY_USERID = "com.example.cst338hw4_gymlog.MAIN_ACTIVITY_USER_ID";
     public static final String SHARED_PREFERENCE_USERID_KEY = "com.example.cst338hw4_gymlog.SHARED_PREFERENCE_USERID_KEY";
-    public static final String SHARED_PREFERENCE_USERID_VALUE = "com.example.cst338hw4_gymlog.SHARED_PREFERENCE_USERID_VALUE";
+    public static final String SAVED_INSTANCE_STATE_KEY = "asdlfkjasldkfjTooManyStrings";
 
     private static final int LOGGED_OUT = -1;
 
@@ -98,6 +99,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SAVED_INSTANCE_STATE_KEY, loggedInUserID);
+        updateSharedPreference();
+    }
+
     private void insertGymlogRecord() {
         if(exercise.isEmpty()) {
             return;
@@ -137,8 +145,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loginUser() {
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(SHARED_PREFERENCE_USERID_KEY, Context.MODE_PRIVATE);
-        loggedInUserID = sharedPreferences.getInt(SHARED_PREFERENCE_USERID_VALUE, LOGGED_OUT);
+        SharedPreferences sharedPreferences = getApplicationContext()
+                .getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        loggedInUserID = sharedPreferences.getInt(getString(R.string.preference_user_id_key), LOGGED_OUT);
         if(loggedInUserID != LOGGED_OUT) {
             return;
         }
