4409c4cb72acada0f72652f2442b8e2dec861018
discotechnology <drosenfeld@csumb.edu>
2025-04-14 14:43:05 -0700

    Added constructor and method to retrieve LiveData logs

--- app/src/main/java/com/example/cst338hw4_gymlog/ViewHolders/GymLogViewModel.java
+++ app/src/main/java/com/example/cst338hw4_gymlog/ViewHolders/GymLogViewModel.java
@@ -1,6 +1,26 @@
 package com.example.cst338hw4_gymlog.ViewHolders;
 
+import android.app.Application;
+
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.cst338hw4_gymlog.database.GymLogRepository;
+import com.example.cst338hw4_gymlog.database.entities.GymLog;
+
+import java.util.List;
 
 public class GymLogViewModel extends AndroidViewModel {
+    private GymLogRepository repository;
+
+    private final LiveData<List<GymLog>> allLogsByID = null;
+    public GymLogViewModel (Application application, int userID) {
+        super(application);
+        repository = GymLogRepository.getRepository(application);
+        allLogsByID = repository.getAllLogsByUserID(userID);
+    }
+
+    public LiveData<List<GymLog>> getAllLogsByID() {
+        return allLogsByID;
+    }
 }
