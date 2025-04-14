544643e88f9586d339a56f4480578b4b47cdf22e
discotechnology <drosenfeld@csumb.edu>
2025-04-14 15:47:58 -0700

    Created GymLogViewHolder

---
+++ app/src/main/java/com/example/cst338hw4_gymlog/ViewHolders/GymLogViewHolder.java
@@ -0,0 +1,32 @@
+package com.example.cst338hw4_gymlog.ViewHolders;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cst338hw4_gymlog.R;
+
+public class GymLogViewHolder extends RecyclerView.ViewHolder {
+
+    private final TextView gymLogViewItem;
+
+    private GymLogViewHolder (View gymLogView) {
+        super(gymLogView);
+        gymLogViewItem = gymLogView.findViewById(R.id.recyclerItemTextView);
+    }
+
+    public void bind (String text) {
+        gymLogViewItem.setText(text);
+    }
+
+    static GymLogViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.gymlog_recycler_item, parent, false);
+
+        return new GymLogViewHolder(view);
+    }
+}
